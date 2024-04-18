d4d1dcc2828d1c0d2b31ff6fe8be16a5c0d99a36
pratiksha <paipratisha8@gmail.com>
2024-04-18 16:25:50 -0400

    cleanup

---
+++ docker/history_for_muse.sh
@@ -0,0 +1,70 @@
+clear
+which python 
+which python3
+apt-get update && apt-get install -y     software-properties-common     wget     ca-certificates     build-essential     libstdc++6     libgcc-s1     libc6     libssl-dev
+which python3
+clear
+which pip
+which pip3
+add-apt-repository ppa:deadsnakes/ppa
+apt-get update && apt-get install -y python3.11 python3.11-venv python3.11-dev python3-pip
+dpkg-reconfigure tzdata
+wget -O liblsl.deb https://github.com/sccn/liblsl/releases/download/v1.16.2/liblsl-1.16.2-jammy_amd64.deb && dpkg -i liblsl.deb && rm liblsl.deb
+apt-get update && apt-get install -y libpugixml1v5
+ls
+cd 
+which python3
+python3
+python3 -m pip install pylsl
+python3
+python3 -m pip install muselsl
+python3
+muselsl record_direct --duration 5 --filename output.csv
+df -h /var/lib/containers/
+history 
+
+podman run --privileged ...
+
+
+
+1  cat /etc/os-release 
+2  uname -m
+3  dpkg --print-architecture
+4  exit
+5  clear
+6  which python 
+7  which python3
+8  apt-get update && apt-get install -y     software-properties-common     wget     ca-certificates     build-essential     libstdc++6     libgcc-s1     libc6     libssl-dev
+9  which python3
+10  clear
+11  which pip
+12  which pip3
+13  add-apt-repository ppa:deadsnakes/ppa
+14  apt-get update && apt-get install -y python3.11 python3.11-venv python3.11-dev python3-pip
+15  dpkg-reconfigure tzdata
+16  wget -O liblsl.deb https://github.com/sccn/liblsl/releases/download/v1.16.2/liblsl-1.16.2-jammy_amd64.deb     && dpkg -i liblsl.deb     && rm liblsl.deb
+17  apt-get update && apt-get install -y libpugixml1v5
+18  ls
+19  cd 
+20  which python3
+21  python3
+22  python3 -m pip install pylsl
+23  python3
+24  python3 -m pip install muselsl
+25  python3
+26  muselsl record_direct --duration 5 --filename output.csv
+27  df -h /var/lib/containers/
+28  history 
+29  clear
+30  muselsl record_direct --duration 5 --filename output.csv
+31  bluetoothctl scan on
+32  apt-get update
+33  apt-get install -y bluez
+34  bluetoothctl scan on
+35  apt-get update && apt-get install -y dbus bluez bluez-tools
+36  service dbus start
+37  bluetoothctl scan on
+38  clear
+39  exit
+40  clear
+41  history 
